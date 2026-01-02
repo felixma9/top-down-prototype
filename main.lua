30a70206428f68b8a3826362ca41629f5b2b2ddf
Felix Ma <112669786+felixma9@users.noreply.github.com>
2026-01-01 20:12:02 -0800

    Added dampening and soft max physics

--- main.lua
+++ main.lua
@@ -5,14 +5,21 @@ local ball = {
     v_x = 0,
     v_y = 0,
     a_x = 0,
-    a_y = 0
+    a_y = 0,
+    r = 20,
+    r_dz = 0,
+    l = 60
 }
 
-local k = 50
-local damping = 0.8
+local k = 70
+local k_max = 500
+local damping = 10
 
 function love.draw()
-    love.graphics.circle("fill", ball.x, ball.y, 10)
+    love.graphics.print("Velocity: "..math.sqrt(ball.v_x * ball.v_x + ball.v_y * ball.v_y))
+
+    love.graphics.circle("fill", ball.x, ball.y, ball.r)
+    love.graphics.circle("line", ball.x, ball.y, ball.r_dz)
 end
 
 function love.load()
@@ -21,20 +28,42 @@ end
 
 function love.update(dt)
     local mouse_x, mouse_y = love.mouse.getPosition()
-    local displacement_x = mouse_x - ball.x
-    local displacement_y = mouse_y - ball.y
+
+    local d_x = mouse_x - ball.x
+    local d_y = mouse_y - ball.y
+
+    local displacement_x = 0
+    local displacement_y = 0
+    local distance = math.sqrt(d_x * d_x + d_y * d_y)
+
+    -- Dead zone
+    if distance > ball.r_dz then
+        local scale = (distance - ball.r_dz) / distance
+        displacement_x = d_x * scale
+        displacement_y = d_y * scale
+    end
 
     -- Calculate acceleration
     ball.a_x = (k * displacement_x) / ball.m
     ball.a_y = (k * displacement_y) / ball.m
+    
+    if distance > ball.l then
+        local excess = distance - ball.l
+        local n_x = d_x / distance
+        local n_y = d_y / distance
+
+        ball.a_x = ball.a_x + k_max * excess * n_x / ball.m
+        ball.a_y = ball.a_y + k_max * excess * n_y / ball.m
+    end
 
     -- Calculate velocity
     ball.v_x = ball.v_x + (ball.a_x * dt)
     ball.v_y = ball.v_y + (ball.a_y * dt)
 
     -- Apply damping
-    ball.v_x = ball.v_x - (ball.v_x * damping * dt)
-    ball.v_y = ball.v_y - (ball.v_y * damping * dt)
+    local decay = math.exp(-damping * dt)
+    ball.v_x = ball.v_x * decay
+    ball.v_y = ball.v_y * decay
     
     -- Calculate the ball's new position
     ball.x = ball.x + (ball.v_x * dt)
